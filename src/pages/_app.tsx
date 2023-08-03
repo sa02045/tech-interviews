c1e7df27b5f3f7bd570a4af66f03e3bb9630d981
sa02045 <sa02045@naver.com>
2023-08-04 00:08:47 +0900

    remove: next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,14 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/react";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <NextUIProvider>
       <ClerkProvider {...pageProps}>
-        <SessionProvider session={session}>
-          <Component {...pageProps} />
-        </SessionProvider>
+        <Component {...pageProps} />
       </ClerkProvider>
     </NextUIProvider>
   );

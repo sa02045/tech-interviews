f530c498c886b21613ee6461595fced0b2ee0a43
sa02045 <sa02045@naver.com>
2023-08-04 23:04:00 +0900

    api: add get post by category

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,10 +3,26 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
   createPost: publicProcedure
-    .input(z.object({ title: z.string(), content: z.string() }))
+    .input(
+      z.object({ title: z.string(), content: z.string(), category: z.string() })
+    )
     .mutation(async ({ input, ctx }) => {
       const post = { ...input };
       await ctx.prisma.post.create({ data: post });
       return post;
+    }),
+
+  getAllPosts: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany();
+    return posts;
+  }),
+
+  getPostsByCategory: publicProcedure
+    .input(z.object({ category: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const posts = await ctx.prisma.post.findMany({
+        where: { category: input.category },
+      });
+      return posts;
     }),
 });
